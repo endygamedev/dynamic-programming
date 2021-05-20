6cbb9af79da2b2bca71c25d7b17e579839a4d0d6
Egor <bronnikov.40@mail.ru>
2021-05-21 00:03:25 +0300

    :memo: Refactor code

--- src/levenshtein-distance.c
+++ src/levenshtein-distance.c
@@ -9,14 +9,14 @@ int min(int x, int y) {
 
 int levenshtein_distance_rec(char * x_string, int m, char * y_string, int n) {
 
-	if (m == 0)
+	if(m == 0)
 		return n;
 
-	if (n == 0)
+	if(n == 0)
 		return m;
     
 	int cost;
-	if (x_string[m - 1] == y_string[n - 1]) {
+	if(x_string[m - 1] == y_string[n - 1]) {
 		cost = 0;
 	} else {
 		cost = 1;
@@ -30,18 +30,17 @@ int levenshtein_distance_rec(char * x_string, int m, char * y_string, int n) {
 int levenshtein_distance_arr(char * x_string, int m, char * y_string, int n) {
 	char D[m + 1][n + 1];
 	memset(D, 0, sizeof(D));
-
-	for (int i = 1; i <= m; i++)
-		D[i][0] = i;
-
-	for (int i = 1; i <= n; i++)
-		D[0][i] = i;
-
 	int sub_cost;
 
-	for (int i = 1; i <= m; i++) {
-		for (int j = 1; j <= n; j++) {
-			if (x_string[i - 1] == y_string[j - 1]){
+	for(int i = 1; i <= m; i++)
+		D[i][0] = i;
+
+	for(int i = 1; i <= n; i++)
+		D[0][i] = i;
+
+	for(int i = 1; i <= m; i++) {
+		for(int j = 1; j <= n; j++) {
+			if(x_string[i - 1] == y_string[j - 1]){
 				sub_cost = 0;
 			} else {
 				sub_cost = 1;
--- src/minimum-cost-to-reach-the-last-cell.c
+++ src/minimum-cost-to-reach-the-last-cell.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 
+
 int min(int x, int y) {
 	return x > y ? y : x;
 }
@@ -12,13 +13,13 @@ int find_min_matrix_path(int costs[5][5]) {
     memset(C, 0, sizeof(C));
     C[0][0] = costs[0][0];
 
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 5; j++) {
-            if (i == 0 && j > 0) {
+    for(int i = 0; i < 5; i++) {
+        for(int j = 0; j < 5; j++) {
+            if(i == 0 && j > 0) {
                 C[0][j] = C[0][j - 1] + costs[0][j];
-            } else if (j == 0 && i > 0) {
+            } else if(j == 0 && i > 0) {
                 C[i][0] += C[i - 1][0] + costs[i][0];
-            } else if (i > 0 && j > 0) {
+            } else if(i > 0 && j > 0) {
                 C[i][j] += min(C[i - 1][j], C[i][j - 1]) + costs[i][j];
             }
         }
