aaf9b07b103d4cef5861fca7a79284067243fae5
arti-shok <60935386+arti-shok@users.noreply.github.com>
2021-05-22 19:25:31 +0300

    :alien: Аdded some algorithms and restructured the project
    Added algorithms such as Count number of paths in a matrix with given cost to reach destination cell, Find all N-digit binary strings without any consecutive 1’s and Rod Cutting Problem!

---
+++ src/rod-cutting-problem.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+int rod_cut(int costs[], int n){ //task 24
+
+    int C[n + 1];
+    memset(C, 0, sizeof(C));
+
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= i; j++){
+
+            C[i] = max(C[i], costs[j - 1] + C[i - j]);
+        }
+    }
+
+    return C[n];
+}
+
+int main(void) {
+    int price[] = {1, 5 ,8, 9, 10, 17, 20};
+    printf("%d", rod_cut(price, 4)); //10
+}
