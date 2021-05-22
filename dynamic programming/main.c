06846537b7cb5cf865f398c75346250f2280323e
arti-shok <60935386+arti-shok@users.noreply.github.com>
2021-05-22 18:24:44 +0300

    :pleading_face:

--- dynamic programming/main.c
+++ dynamic programming/main.c
@@ -1,6 +1,11 @@
+int arr_len(int * x) {
+    return sizeof(x)/sizeof(x[0]);
+}
+
 int main()
 {
-    printf("ooooo");
+    int* y = {1, 1, 1, 1, 1};
+    printf("%d\n", arr_len(y));
     return 0;
 }
 
